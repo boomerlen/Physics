289bd7d17e75f6d24c23030abc637aefcf81db34
boomerlen <bunnieweirdo@gmail.com>
2021-08-25 14:10:43 +1000

    some bug fixes

--- include/Node_T.h
+++ include/Node_T.h
@@ -1,6 +1,8 @@
 #ifndef NODE_T_H
 #define NODE_T_H
 
+#include <iostream>
+
 template <class T>
 class Node_T
 {
@@ -19,3 +21,36 @@ class Node_T
 };
 
 #endif // NODE_T_H
+
+// Implementations
+
+template <class T>
+Node_T<T>::Node_T()
+{
+    next = nullptr;
+    initialised = false;
+}
+
+// Should throw an exception if it still points to something
+template <class T>
+Node_T<T>::~Node_T()
+{
+    //dtor
+    if (next != nullptr) {
+        // problem
+        // TODO finish this
+        // This should throw an exception
+    }
+}
+
+template <class T>
+void Node_T<T>::set_data(T d) {
+    std::cout << "setting data in NOde_T.h" << std::endl;
+    data = d;
+    initialised = true;
+}
+
+template <class T>
+T Node_T<T>::get_data() {
+    return data;
+}
